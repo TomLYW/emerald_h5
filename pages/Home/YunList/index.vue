4bdad1bf2388564cada92d2332f436716ffddec0
liaoyang <317378719@qq.com>
2023-04-13 01:55:10 +0800

    4.12.1

--- pages/Home/YunList/index.vue
+++ pages/Home/YunList/index.vue
@@ -2,16 +2,24 @@
 	<view class="container">
 		<view class="cell">
 			<view class="tag">
-				<text class="tag-title">环境发生</text>
+				<text class="tag-title">经典套餐</text>
 			</view>
 
-			<!-- <text>环境事件的发生</text>
-			<text>环境事件的发生</text> -->
+			<view class="caption">
+				<view class=""></view>
+				<text></text>
+			</view>
+			<view class="line" />
+			<view class="info"></view>
 		</view>
 	</view>
 </template>
 
 <script setup>
+	import {
+		ref
+	} from 'vue';
+	const px = uni.getLocale() === 'cn' ? '12px' : '4px';
 </script>
 
 <style lang="scss" scoped>
@@ -23,18 +31,16 @@
 		// display: flex;
 		// flex-direction: column;
 		height: 150px;
-		border-radius: 12px;
+		border-radius: 15px;
 		background-color: #E5E5E5;
-		box-shadow: 0px 0px 10px -8px #000;
+		box-shadow: 0px 0px 10px -6px #000;
 
 		.tag {
-			width: 70px;
-			height: 18px;
+			display: inline-block;
+			padding: 1px v-bind('px');
 			background-color: #05AA84;
 			border-top-left-radius: 15px;
 			border-bottom-right-radius: 15px;
-			line-height: 18px;
-			text-align: center;
 
 			.tag-title {
 				font-size: 12px;
